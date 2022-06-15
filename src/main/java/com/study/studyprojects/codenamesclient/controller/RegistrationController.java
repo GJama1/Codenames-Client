97774195b36feb461430352efc9ce93f2fae9eb1
GJama1 <gjama730@gmail.com>
2022-06-15 12:13:24 +0400

    Implement connection with the server

--- src/main/java/com/study/studyprojects/codenamesclient/controller/RegistrationController.java
+++ src/main/java/com/study/studyprojects/codenamesclient/controller/RegistrationController.java
@@ -7,6 +7,7 @@ public class RegistrationController {
 
 
 
+
     @FXML
     public void onSignupButtonClick() {
 
---
+++ src/main/java/com/study/studyprojects/codenamesclient/utils/SocketStreams.java
@@ -0,0 +1,11 @@
+package com.study.studyprojects.codenamesclient.utils;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SocketStreams {
+
+    public static ObjectOutputStream out;
+    public static ObjectInputStream in;
+
+}
